56b3a59a2d28a08f7448f4065f9c9d7595696d3c
dochoi <ehdrb345@naver.com>
2020-05-02 19:51:54 +0900

    Q

---
+++ day00/ex11/other_costs.py
@@ -0,0 +1,49 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    other_costs.py                                     :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: dochoi <dochoi@student.42seoul.kr>         +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/05/02 19:42:58 by dochoi            #+#    #+#              #
+#    Updated: 2020/05/02 19:46:27 by dochoi           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def mse_(y, y_hat):
+    """ Description: Calculate the MSE between the predicted output and the real output.
+    Args:
+    y: has to be a numpy.ndarray, a vector of dimension m * 1.
+    y_hat: has to be a numpy.ndarray, a vector of dimension m * 1.
+    Returns:
+    mse: has to be a float.
+    None if there is a matching dimension problem.
+    Raises: This function should not raise any Exceptions. """
+
+def rmse_(y, y_hat):
+    """ Description: Calculate the RMSE between the predicted output and the real output.
+    Args:
+    y: has to be a numpy.ndarray, a vector of dimension m * 1.
+    y_hat: has to be a numpy.ndarray, a vector of dimension m * 1.
+    Returns:
+    rmse: has to be a float.
+    None if there is a matching dimension problem.
+    Raises: This function should not raise any Exceptions. """
+
+def mae_(y, y_hat):
+    """ Description: Calculate the MAE between the predicted output and the real output.
+    Args:
+    y: has to be a numpy.ndarray, a vector of dimension m * 1.
+    y_hat: has to be a numpy.ndarray, a vector of dimension m * 1.
+    Returns:
+    mae: has to be a float. None if there is a matching dimension problem.
+    Raises: This function should not raise any Exceptions. """
+
+def r2score_(y, y_hat):
+    """ Description: Calculate the R2score between the predicted output and the output.
+    Args:
+    y: has to be a numpy.ndarray, a vector of dimension m * 1.
+    y_hat: has to be a numpy.ndarray, a vector of dimension m * 1.
+    Returns: r2score: has to be a float.
+    None if there is a matching dimension problem. Raises:
+    This function should not raise any Exceptions. """
